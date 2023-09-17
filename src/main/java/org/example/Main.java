b42aa779b3d9ad2a192957eda66531728d87c1c5
AlexLojko <a_l_g@tut.by>
2023-09-17 23:43:11 +0300

    add test

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,45 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        ToyShop toyShop = new ToyShop();
+
+        // Добавление новых игрушек
+        Toy toy1 = new Toy(1, "Кукла", 3, 50);
+        Toy toy2 = new Toy(2, "Мяч", 5, 30);
+        Toy toy3 = new Toy(3, "Машинка", 2, 70);
+
+        toyShop.addToy(toy1);
+        toyShop.addToy(toy2);
+        toyShop.addToy(toy3);
+
+        // Изменение веса выпадения игрушек
+        toyShop.changeWeight(15, 60);
+
+        // Организация розыгрыша игрушек
+        toyShop.organizeGiveaway();
+        toyShop.organizeGiveaway();
+
+
+
+        // Получение призовой игрушки
+        Toy prizeToy = toyShop.getPrizeToy();
+        if (prizeToy != null) {
+            System.out.println("Вы получили призовую игрушку: " + prizeToy.getName());
+        } else {
+            System.out.println("Призовые игрушки закончились");
+        }
+        prizeToy = toyShop.getPrizeToy();
+        if (prizeToy != null) {
+            System.out.println("Вы получили призовую игрушку: " + prizeToy.getName());
+        } else {
+            System.out.println("Призовые игрушки закончились");
+        }
+        prizeToy = toyShop.getPrizeToy();
+        if (prizeToy != null) {
+            System.out.println("Вы получили призовую игрушку: " + prizeToy.getName());
+        } else {
+            System.out.println("Призовые игрушки закончились");
+        }
+    }
+}
---
+++ src/main/java/org/example/ToyShop.java
@@ -0,0 +1,61 @@
+package org.example;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+class ToyShop {
+    private List<Toy> toys;
+    private List<Toy> prizeToys;
+
+    public ToyShop() {
+        toys = new ArrayList<>();
+        prizeToys = new ArrayList<>();
+    }
+
+    public void addToy(Toy toy) {
+        toys.add(toy);
+    }
+
+    public void changeWeight(int toyId, int weight) {
+        for (Toy toy : toys) {
+            if (toy.getId() == toyId) {
+                toy.setWeight(weight);
+                break;
+            }
+        }
+        System.out.println("Нет такой игрушки!!");
+    }
+
+    public void organizeGiveaway() {
+        Random random = new Random();
+        for (Toy toy : toys) {
+            int randomNum = random.nextInt(100);
+            if (randomNum < toy.getWeight()) {
+                prizeToys.add(toy);
+                toy.setQuantity(toy.getQuantity() - 1);
+            }
+        }
+    }
+
+    public Toy getPrizeToy() {
+        if (prizeToys.size() > 0) {
+            Toy prizeToy = prizeToys.get(0);
+            prizeToys.remove(0);
+            writeToFile(prizeToy);
+            return prizeToy;
+        } else {
+            return null;
+        }
+    }
+
+    private void writeToFile(Toy toy) {
+        try (FileWriter writer = new FileWriter("prize_toys.txt", true)) {
+            writer.write(toy.getName() + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
